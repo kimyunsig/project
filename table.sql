e370738442d7aeada08789de11d9afc24fbf4a83
kimyunsig <147125471+kimyunsig@users.noreply.github.com>
2023-10-16 09:46:31 +0900

    Update table.sql

--- table.sql
+++ table.sql
@@ -7,18 +7,22 @@ CREATE TABLE Users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 select * From users;
--- 예약 정보 저장 테이블
-CREATE TABLE Reservations (
+
+
+-- 예약 테이블
+CREATE TABLE reservations (
     reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    reservation_date DATE,
-    start_time TIME,
-    end_time TIME,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    user_id INT NOT NULL,  -- 예약한 사용자 ID
+    field_id INT NOT NULL,  -- 풋살장 ID
+    reservation_date DATE NOT NULL,  -- 예약한 날짜
+    start_time TIME NOT NULL,  -- 예약 시작 시간
+    end_time TIME NOT NULL,  -- 예약 종료 시간
+    reservation_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,  -- 예약 생성 일자 및 시간
+    FOREIGN KEY (user_id) REFERENCES users(user_id),  -- 사용자 테이블과 연결
+    FOREIGN KEY (field_id) REFERENCES field(field_id)  -- 풋살장 테이블과 연결
 );
 
-SELECT * FROM reservations;
+select * from reservations;
 
 
 
